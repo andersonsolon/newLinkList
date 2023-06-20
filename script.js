b321ad94b572dcafc9f9cec60079b1e3a91c5b5e
Anderson Solon <anderson.solon@gmail.com>
2023-06-20 19:57:39 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function toogleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //pegar a imagem
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "./assets/Avatar.png")
+  }
+}
